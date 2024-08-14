5074f440dd31ffa3823a05428b497b429a0006c5
arose20 <antonyrose17@gmail.com>
2024-08-15 00:40:14 +0100

    Added example2 for not in activated env

---
+++ Not_in_activated_virtual_environment/example_2/run_execute_notebook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Prompt the user for input
+#read -p "Enter the kernel name: " kernel_name
+#read -p "Enter the template file: " template_file
+#read -p "Enter the output file name: " output_file
+#read -p "Enter the output log name: " log_file
+#read -p "Enter the timout for code block execution: " code_timeout
+
+# Pre-assign the variables
+kernel_name="notebook_inheritor_env"
+template_file="Template_notebook_2.ipynb"
+output_file="output_notbook.ipynb"
+log_file="job_logger.log"
+code_timeout="172800" # measured in seconds - default 600
+
+# Run the Python script
+python execute_notebook.py "$kernel_name" "$template_file" "$output_file" "$log_file" "$code_timeout"
